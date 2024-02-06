9876423bc4a6ac3795fb340ac9269d18592bc9c7
Damian <ruiz.damian17@gmail.com>
2024-02-06 11:27:07 -0500

    Cleanup

--- src/reducers/teamsReducer.tsx
+++ src/reducers/teamsReducer.tsx
@@ -4,9 +4,7 @@ export const teamsReducer = (state: Team[], action: TeamsAction) => {
   const modifyScore = (modifier: TeamsActionType) => {
     const newState = [...state];
     const team = newState[action.teamId];
-
     team.score = modifier == 'increment' ? team.score + 1 : team.score - 1;
-
     return newState;
   };
 
